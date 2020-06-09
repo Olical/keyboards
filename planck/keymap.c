a9c6009730b12eda24e518393767d7d8661e4ba6
Oliver Caldwell <ollie@oli.me.uk>
2020-06-09 10:55:51 +0100

    Swap enter and shift keys

--- planck/keymap.c
+++ planck/keymap.c
@@ -90,14 +90,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |   {  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |   #  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  | Tab  | Lower|    Space    | Raise| Bksp | Esc  | Enter| Shift|
+ * | Ctrl | GUI  | Alt  | Tab  | Lower|    Space    | Raise| Bksp | Esc  | Shift| Enter|
  * `-----------------------------------------------------------------------------------'
  */
 [_DEFAULT] = {
   {KC_LBRC, KC_Q,    KC_W,    KC_E,   KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_EQL},
   {KC_LPRN, KC_A,    KC_S,    KC_D,   KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LCBR, KC_Z,    KC_X,    KC_C,   KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_NUHS},
-  {KC_LCTL, KC_LGUI, KC_LALT, KC_TAB, LOWER, KC_SPC, KC_SPC, RAISE, KC_BSPC, KC_ESC,  KC_ENT,  KC_LSFT}
+  {KC_LCTL, KC_LGUI, KC_LALT, KC_TAB, LOWER, KC_SPC, KC_SPC, RAISE, KC_BSPC, KC_ESC,  KC_LSFT, KC_ENT}
 },
 
 /* Lower
